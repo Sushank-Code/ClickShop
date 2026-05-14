7a96b6215b0d4408357b4ec156449c539beb7f22
Sushank-Code <sushanklamsal7979@gmail.com>
2026-05-15 02:13:02 +0545

    Redesign UI

--- eproject/kartapp/views.py
+++ eproject/kartapp/views.py
@@ -4,7 +4,7 @@ from store.models import Product,ReviewRating
 # Create your views here.
 
 def HomeView(request):
-    products = Product.objects.filter(is_available = True)
+    products = Product.objects.filter(is_available = True).order_by('-created_date')[:15]
     
     context = {
         'products' : products ,
